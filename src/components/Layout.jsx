a2091efc9eb4099f2b27d00e0d41eced93af4e62
Ruchi <ruchi.thanabhawan@gmail.com>
2024-07-06 08:49:11 -0700

    Add navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 export default function Layout() {
   return (
     <>
+      <Navbar />
       <div className="container mx-auto">
         <Outlet />
       </div>
