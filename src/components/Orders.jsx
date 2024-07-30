51f1d465740028317ee77bb0b1f4df5aa095de70
Ruchi <ruchi.thanabhawan@gmail.com>
2024-07-29 22:21:31 -0700

    fetch orders

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Orders() {
+  const [orders, setOrders] = useState([]);
+
+    
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  function fetchOrders() {
+    axios.get(`http://localhost:8080/orders/1`).then((response) => {
+      setOrders(response.data);
+      console.log(response.data);
+    });
+  }
+
   return (
-    <div>
-      Orders
+  <div>
+{
+  orders.map((order) => (
+    <div key={order.id}>
+      <p>{order.total_price}</p>
+      <p>{order.quantity}</p>
+      <p>{order.status}</p>
     </div>
+  ))
+}
+  </div>
   )
 }
 
-export default Orders
+export default Orders;
